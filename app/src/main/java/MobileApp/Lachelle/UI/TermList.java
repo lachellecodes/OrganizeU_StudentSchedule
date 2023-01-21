6b458369095e9c86226a1bf17478030d088c6ac1
Lachelle <lachelleallen@icloud.com>
2023-01-21 14:42:12 -0600

    initial commit

---
+++ app/src/main/java/MobileApp/Lachelle/UI/TermList.java
@@ -0,0 +1,16 @@
+package MobileApp.Lachelle.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import MobileApp.Lachelle.R;
+
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
+}
