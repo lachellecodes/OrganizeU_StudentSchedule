d01f365df640a3147a456db982cb53aa9fbd1c91
Lachelle <lachelleallen@icloud.com>
2023-01-21 19:22:27 -0600

    4 UI screens added

---
+++ app/src/main/java/MobileApp/Lachelle/UI/ClassDetails.java
@@ -0,0 +1,22 @@
+package MobileApp.Lachelle.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import MobileApp.Lachelle.R;
+
+public class ClassDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_details);
+
+    }
+}
+
--- app/src/main/java/MobileApp/Lachelle/UI/TermDetails.java
+++ app/src/main/java/MobileApp/Lachelle/UI/TermDetails.java
@@ -5,24 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import MobileApp.Lachelle.R;
 
-public class TermList extends AppCompatActivity {
+public class TermDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_term_list);
-        FloatingActionButton fab=findViewById(R.id.floatingActionButton2);
+        setContentView(R.layout.activity_term_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TermList.this, TermDetails.class);
+                Intent intent = new Intent (TermDetails.this, ClassDetails.class);
                 startActivity(intent);
             }
         });
     }
-}
+    }
